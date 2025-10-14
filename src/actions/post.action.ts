07453ec53954298b6359b89a534b56d5c2f61e80
Kealan Heena <kealanheena@gmail.com>
2025-10-14 00:54:19 -0700

    Feat: Create functionality for the getPosts function

--- src/actions/post.action.ts
+++ src/actions/post.action.ts
@@ -15,6 +15,10 @@ export async function createPost(data: PostCreationData) {
 	try {
 		const userId = await getDbUserId();
 
+		if (!userId) {
+			return;
+		}
+
 		const post = await prisma.post.create({
 			data: {
 				content,
@@ -35,5 +39,52 @@ export async function createPost(data: PostCreationData) {
 }
 
 export async function getPosts() {
-	
+	try {
+		const posts = await prisma.post.findMany({
+			orderBy: {
+				createdAt: 'desc'
+			},
+			include: {
+				author: {
+					select: {
+						name: true,
+						image: true,
+						username: true
+					}
+				},
+				comments: {
+					orderBy: {
+						createdAt: 'asc'
+					},
+					include: {
+						author: {
+							select: {
+								id: true,
+								name: true,
+								image: true,
+								username: true
+							}
+						}
+					}
+				},
+				likes: {
+					select: {
+						userId: true,
+					}
+				},
+				_count: {
+					select: {
+						likes: true,
+						comments: true,
+					}
+				}
+			}
+		});
+
+		return posts;
+	} catch (error) {
+		console.error('Error in getPosts', error);
+
+		throw new Error('Failed to fetch posts')
+	}
 }
