1b611f4a4c8cebcb51f482f84c279318c0e23fe1
Kealan Heena <kealanheena@gmail.com>
2025-10-14 00:40:47 -0700

    Feat: create PostCard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
 import { currentUser } from "@clerk/nextjs/server";
 
+import { getPosts } from "@/actions/post.action";
 import CreatePost from "@/components/CreatePost";
 import WhoToFollow from "@/components/WhoToFollow";
+import PostCard from "@/components/PostCard";
 
 export default async function Home() {
   const user = await currentUser();
-
+  const posts = await getPosts();
 
   return (
     <div className='grid grid-cols-1 lg:grid-cols-10 gap-6'>
       <div className='lg:col-span-6'>
         {user ? <CreatePost /> : null}
+        <div className="space-y-6">
+          {posts.map(post => (
+            <PostCard key={posts.id} post={post} />
+          ))}
+        </div>
       </div>
 
       <div className='hidden lg:block col-span-4 sticky top-20'>
