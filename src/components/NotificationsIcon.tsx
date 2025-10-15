e0e26fcad4b7178062ad6ada426d336d452933c1
Kealan Heena <kealanheena@gmail.com>
2025-10-15 16:51:31 -0700

    Feat: add list mapping to the notifications page

---
+++ src/components/NotificationsIcon.tsx
@@ -0,0 +1,23 @@
+@ -0,0 +1,22 @@
+"use client"
+
+import React from 'react';
+import { HeartIcon, MessageCircleIcon, UserPlusIcon } from "lucide-react";
+
+function NotificationIcon({ type }: { type: string | null }) {
+	if (type === 'LIKE') {
+		return <HeartIcon className="size-4 text-red-500" />;
+	}
+
+	if (type === 'COMMENT') {
+		return <MessageCircleIcon className="size-4 text-blue-500" />;
+	}
+
+	if (type === 'FOLLOW') {
+		return <UserPlusIcon className="size-4 text-green-500" />;
+	}
+
+	return null;
+}
+
+export default NotificationIcon;
