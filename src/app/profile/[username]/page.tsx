4208de623bdeb607e46481568462136768cce4d2
Kealan Heena <kealanheena@gmail.com>
2025-10-16 12:06:29 -0700

    Feat: add generateMetadata function to profile page

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,3 +1,17 @@
+import { getProfileByUsername } from "@/actions/profile.action";
+
+export async function generateMetadata({ params }: { params: { username: string } }) {
+	const user = getProfileByUsername(params.username)
+
+	if (!user) {
+		return;
+	}
+
+	return {
+		title: `Profile | ${user.name || params.username}`,
+		description: user.bio || `Checkout out ${params.username}'s profile!`,
+	}
+}
 
 function ProfilePage({ params }: { params: { username: string } }) {
 	console.log({ params });
